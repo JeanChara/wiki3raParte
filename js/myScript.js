9d42d6de14adfac411d51a860a9191f71b2ec013
JeanChara <jcharaco@unsa.edu.pe>
2022-12-25 19:41:12 -0500

    funcionalidad a doNew()

--- js/myScript.js
+++ js/myScript.js
@@ -227,7 +227,8 @@ function showNew(){
 
     let showNew = `<p>Titulo</p>
     <input id='titulo' name ='titulo' type='text'><br>
-    <textarea style = "width: 100%;" type="text" name="cuerpo"></textarea><br>
+    <p>Contenido-markdown</p>
+    <textarea style = "width: 100%;" type="text" id ="cuerpo" name="cuerpo"></textarea><br>
     <button onclick='doNew()'>Enviar</button>
     <button onclick='doList()'>Cancelar</button>`;
   
@@ -242,14 +243,22 @@ function showNew(){
  * función responseNew
  */
 function doNew(){
+    // encodeURI reemplaza espacios con %20, entonces reemplazamos %20 con + para la url
+    let titulo = encodeURIComponent(document.getElementById("titulo").value).replace(/%20/g, "+"); // para reemplazar espacios en el url
+    
+    let cuerpo = encodeURIComponent(document.getElementById("cuerpo").value).replace(/%20/g,"+");
+    
+    let url = "http://192.168.1.6/~alumno/proyectoFinal/cgi-bin/new.pl?usuario="+userKey+"&titulo="+titulo+"&cuerpo="+cuerpo;
+    console.log(url);
+    let xhr = new XMLHttpRequest();
+      
+    xhr.open("GET", url, true);
+    xhr.send();
 
-
-
-  
-
-
-
-
+    xhr.onload = function () {
+        responseNew(xhr.responseXML);
+    };
+    
 
 }
 
@@ -259,13 +268,6 @@ function doNew(){
  * correspondiera
  */
 function responseNew(response){
-
-
-
-
-
-
-
 
 
 }
